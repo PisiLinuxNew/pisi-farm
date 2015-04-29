634e754931f1f30e1f343d011e94cc308f2d0657
root <root@manaps-kumpir.se>
2015-04-29 17:20:52 -0400

    github auth sonraya birakildi

--- web/flask/app/__init__.py
+++ web/flask/app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.github import GitHub
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-gituser = None
-github = GitHub(app)
 
 from app import routes
--- web/flask/app/routes.py
+++ web/flask/app/routes.py
@@ -1,12 +1,10 @@
 
 from flask import Flask, render_template
 from app import app
-from app import github
-from app import gituser
 
 @app.route('/')
 def home():
-  return render_template('home.html', user = gituser)
+  return render_template('home.html')
 
 @app.route('/about')
 def about():
@@ -17,28 +15,6 @@ def queue():
   from app import db
   pkgs=["acl","attr"]
   return render_template('queue.html', packages = pkgs)
-
-@app.route('/login')
-def login():
-  return github.authorize()
-
-@app.route('/github-callback')
-@github.authorized_handler
-def authorized(oauth_token):
-    next_url = request.args.get('next') or url_for('index')
-    if oauth_token is None:
-        flash("Authorization failed.")
-        return redirect(next_url)
-
-    user = User.query.filter_by(github_access_token=oauth_token).first()
-    if user is None:
-        user = User(oauth_token)
-        db_session.add(user)
-    gituser = str(github.get('user'))
-    print "----",gituser
-    user.github_access_token = oauth_token
-    db_session.commit()
-    return redirect(url_for('/'))
 
 if __name__ == '__main__':
   app.run(debug=True)
